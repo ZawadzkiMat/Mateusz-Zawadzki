170c83a6e473fe5dd2052c4376a740949a82feb1
Mateusz Zawadzki <50049321+ZawadzkiMat@users.noreply.github.com>
2019-07-03 21:40:01 +0200

    Add files via upload

---
+++ Okregi/Okregi/Okregi/Okregi/Form1.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace Okregi
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+       
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            
+            
+           
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 753);
+            this.Name = "Form1";
+            this.Text = "Okregi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
